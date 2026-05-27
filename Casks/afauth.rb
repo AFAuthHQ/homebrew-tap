93661f7041baa520b74237a52b2ec74fcf6e8559
Wen Guo <gw3218@126.com>
2026-05-27 17:09:48 +0800

    cask: postflight strips quarantine xattr (unbreaks v0.2.1 on macOS)

--- Casks/afauth.rb
+++ Casks/afauth.rb
@@ -34,6 +34,12 @@ cask "afauth" do
 
   binary "afauth"
 
+  postflight do
+    if OS.mac?
+      system_command "/usr/bin/xattr", args: ["-dr", "com.apple.quarantine", "#{staged_path}/afauth"]
+    end
+  end
+
   # No zap stanza required
 
 end
